0b765e1e1022480c97ca1f07a508410fb1a2793d
@telecristy <telematicacristina@gmail.com>
2020-06-03 13:23:41 -0700

    Implementing local state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+// Dependencies
+import React, { useState } from 'react';
+
+// Images
 import logo from './logo.svg';
+
+// Style
 import './App.css';
 
 function App() {
+  // Local state
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
